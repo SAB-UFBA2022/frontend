7d7075cfa039b63a44d61e1ffe9bdcbf6f1ab256
Monique Silva <moniquesntsilva@gmail.com>
2022-09-18 12:36:56 -0300

    feat: adiciona nova regra

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     'react/jsx-props-no-spreading': 'off',
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'eslint-disable-line-camelcase': 'off',
+    camelcase: 'off'
   }
 }

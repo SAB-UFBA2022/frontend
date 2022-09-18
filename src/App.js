abe2fc7f3109441d369555092c0615ddda52229b
Monique Silva <moniquesntsilva@gmail.com>
2022-09-18 12:39:05 -0300

    feat: adiciona toastify e auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import AuthProvider from './contexts/Auth/AuthProvider'
 import Login from './pages/Login'
+import UserDashboard from './pages/User/Dashboard'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/user/dashboard" element={<UserDashboard />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer autoClose={3000} />
+    </AuthProvider>
   )
 }

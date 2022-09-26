70c86870679ffc47fed9128bd43444d5249ff257
Monique Silva <moniquesntsilva@gmail.com>
2022-09-26 16:19:49 -0300

    feat: adiciona nova pagina

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,8 @@
 import AdminDashboard from './Admin/Dashboard'
 import DiscenteDashboard from './Discente/Dashboard'
+import DiscenteLista from './Discente/Lista'
 import DocenteDashboard from './Docente/Dashboard'
 import Login from './Login'
 import Page404 from './Page404'
 
-export { AdminDashboard, DiscenteDashboard, DocenteDashboard, Login, Page404 }
+export { AdminDashboard, DiscenteDashboard, DiscenteLista, DocenteDashboard, Login, Page404 }

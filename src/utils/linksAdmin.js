0c199c9effe29d3267c5e0e1ffc70663dcad5344
Monique Silva <moniquesntsilva@gmail.com>
2022-09-30 01:01:07 -0300

    refactor: corrige sidebar

---
+++ src/utils/linksAdmin.js
@@ -0,0 +1,22 @@
+export const linksAdmin = [
+  {
+    name: 'Dashboard',
+    icon: 'home',
+    path: '/discente/dashboard'
+  },
+  {
+    name: 'Estudantes',
+    icon: 'list',
+    path: '/discente/lista-estudantes'
+  },
+  {
+    name: 'Relatórios',
+    icon: 'paper',
+    path: '/discente/relatorios'
+  },
+  {
+    name: 'Perfil',
+    icon: 'profile',
+    path: '/discente/perfil'
+  }
+]

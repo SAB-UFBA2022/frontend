2a27cfa851968372dae0109d133097c4a4572716
Monique Silva <moniquesntsilva@gmail.com>
2022-10-01 23:51:31 -0300

    feat: loading spinner

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,12 @@
+function Loading() {
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className="spinner-border inline-block h-52 w-52 animate-spin rounded-full border-4"
+        role="status"
+      />
+    </div>
+  )
+}
+
+export default Loading

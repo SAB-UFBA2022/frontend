f36f70e3cab75722c424db67fd3fc73b5d20ecd6
Monique Silva <moniquesntsilva@gmail.com>
2022-10-03 13:06:05 -0300

    refactor: muda loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,10 +1,7 @@
 function Loading() {
   return (
-    <div className="flex items-center justify-center">
-      <div
-        className="spinner-border inline-block h-52 w-52 animate-spin rounded-full border-4"
-        role="status"
-      />
+    <div className="flex h-screen items-center justify-center ">
+      <div className="h-40 w-40 animate-spin rounded-full border-t-4 border-b-4 border-gray-800" />
     </div>
   )
 }

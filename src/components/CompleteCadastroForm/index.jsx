0ea26dd3fc7830f5d29be2d74d37270249e51a15
Gabriel Soares <gabrielsoaresm94@gmail.com>
2022-10-05 01:55:46 -0300

    fix: atualização de inputs em completar-cadastro

--- src/components/CompleteCadastroForm/index.jsx
+++ src/components/CompleteCadastroForm/index.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import Button from '../Button'
+import { FormInput, Button } from '..'
+
+const initialState = {
+  matricula_id: ''
+}
 
 export default function CompleteCadastroForm() {
+  const [values, setValues] = useState(initialState)
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+
   const {
     register,
     formState: { errors }
@@ -12,32 +21,22 @@ export default function CompleteCadastroForm() {
 
   return (
     <form className="tablet:grid-cols-2 flex w-full flex-wrap items-center justify-center gap-y-2 font-inter">
-      <label
-        htmlFor="tax_id"
-        className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800"
-      >
-        Número matrícula
-        <input
-          type="number"
-          id="tax_id"
+      <div className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800">
+        <FormInput
+          label="Número matrícula"
+          type="text"
+          id="matricula_id"
+          name="matricula_id"
+          value={values.matricula_id}
           placeholder="Digite seu número de matrícula"
-          aria-invalid={errors.tax_id ? 'true' : 'false'}
           autoComplete="off"
-          {...register('tax_id', {
-            required: {
-              value: true,
-              message: 'Campo Obrigatório.'
-            }
-          })}
-          className={`${
-            errors.nome ? `focus:ring-red-500` : `focus:ring-sky-500`
-          }'placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
-          focus:outline-none focus:ring-1 focus:ring-sky-500 `}
+          handleChange={handleChange}
+          pattern="[0-9]{9}"
+          patternErro="Matrícula inválida, formato esperado: 111111000"
+          className="placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
+        focus:outline-none focus:ring-1 focus:ring-sky-500"
         />
-        <span role="alert" className="font-inter text-xs text-red-500 md:text-sm">
-          {errors?.tax_id?.message}
-        </span>
-      </label>
+      </div>
       <label
         htmlFor="curso_id"
         className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800"
@@ -111,32 +110,22 @@ export default function CompleteCadastroForm() {
           <option value="mestrado">Test</option>
         </select>
       </label>
-      <label
-        htmlFor="curriculo_id"
-        className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800"
-      >
-        Link currículo Lattes
-        <input
-          type="number"
+      <div className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800">
+        <FormInput
+          label="Link currículo Lattes"
+          type="text"
           id="curriculo_id"
+          name="curriculo_id"
+          value={values.curriculo_id}
           placeholder="Link Lattes"
-          aria-invalid={errors.curriculo_id ? 'true' : 'false'}
           autoComplete="off"
-          {...register('curriculo_id', {
-            required: {
-              value: true,
-              message: 'Campo Obrigatório.'
-            }
-          })}
-          className={`${
-            errors.nome ? `focus:ring-red-500` : `focus:ring-sky-500`
-          }'placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
-          focus:outline-none focus:ring-1 focus:ring-sky-500 `}
+          handleChange={handleChange}
+          pattern="(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])"
+          patternErro="Lattes inválido, formato esperado: http://lattes.cnpq.br/9001443717831147"
+          className="placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
+        focus:outline-none focus:ring-1 focus:ring-sky-500"
         />
-        <span role="alert" className="font-inter text-xs text-red-500 md:text-sm">
-          {errors?.curriculo_id?.message}
-        </span>
-      </label>
+      </div>
       <label
         htmlFor="agencia_id"
         className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800"
@@ -160,84 +149,50 @@ export default function CompleteCadastroForm() {
           <option value="mestrado">Test</option>
         </select>
       </label>
-      <label
-        htmlFor="bolsa_id"
-        className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800"
-      >
-        Valor da bolsa
-        <input
-          type="number"
+      <div className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800">
+        <FormInput
+          label="Valor da bolsa"
+          type="text"
           id="bolsa_id"
+          name="bolsa_id"
+          value={values.bolsa_id}
           placeholder="Digite o valor da bolsa"
-          aria-invalid={errors.bolsa_id ? 'true' : 'false'}
           autoComplete="off"
-          {...register('bolsa_id', {
-            required: {
-              value: true,
-              message: 'Campo Obrigatório.'
-            }
-          })}
-          className={`${
-            errors.nome ? `focus:ring-red-500` : `focus:ring-sky-500`
-          }'placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
-          focus:outline-none focus:ring-1 focus:ring-sky-500 `}
+          handleChange={handleChange}
+          pattern="^R\$(\d{1,3}(\.\d{3})*|\d+)(\,\d{2})?$"
+          patternErro="Bolsa inválida, formato esperado: R$1000"
+          className="placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
+        focus:outline-none focus:ring-1 focus:ring-sky-500"
         />
-        <span role="alert" className="font-inter text-xs text-red-500 md:text-sm">
-          {errors?.bolsa_id?.message}
-        </span>
-      </label>
-      <label
-        htmlFor="inicio_id"
-        className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800"
-      >
-        Início da bolsa
-        <input
+      </div>
+      <div className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800">
+        <FormInput
+          label="Início da bolsa"
           type="date"
           id="inicio_id"
+          name="inicio_id"
+          value={values.inicio_id}
           placeholder="Selecione data de início"
-          aria-invalid={errors.inicio_id ? 'true' : 'false'}
           autoComplete="off"
-          {...register('inicio_id', {
-            required: {
-              value: true,
-              message: 'Campo Obrigatório.'
-            }
-          })}
-          className={`${
-            errors.nome ? `focus:ring-red-500` : `focus:ring-sky-500`
-          }'placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
-          focus:outline-none focus:ring-1 focus:ring-sky-500 `}
+          handleChange={handleChange}
+          className="placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
+        focus:outline-none focus:ring-1 focus:ring-sky-500"
         />
-        <span role="alert" className="font-inter text-xs text-red-500 md:text-sm">
-          {errors?.inicio_id?.message}
-        </span>
-      </label>
-      <label
-        htmlFor="fim_id"
-        className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800"
-      >
-        Fim da bolsa
-        <input
+      </div>
+      <div className="flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800">
+        <FormInput
+          label="Fim da bolsa"
           type="date"
           id="fim_id"
+          name="fim_id"
+          value={values.fim_id}
           placeholder="Selecione data de término"
-          aria-invalid={errors.fim_id ? 'true' : 'false'}
           autoComplete="off"
-          {...register('fim_id', {
-            required: {
-              value: true,
-              message: 'Campo Obrigatório.'
-            }
-          })}
-          className={`${
-            errors.nome ? `focus:ring-red-500` : `focus:ring-sky-500`
-          }'placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
-          focus:outline-none focus:ring-1 focus:ring-sky-500 `}
+          handleChange={handleChange}
+          className="placeholder-gray-400::placeholder w-full max-w-[395px] rounded-lg border border-gray-400 px-4 py-3 text-base font-normal leading-6 text-gray-800
+        focus:outline-none focus:ring-1 focus:ring-sky-500"
         />
-        <span role="alert" className="font-inter text-xs text-red-500 md:text-sm">
-          {errors?.fim_id?.message}
-        </span>
-      </label>
+      </div>
       <div className=" flex h-[109px] w-1/3 flex-col gap-y-1.5 px-2 text-base font-medium leading-7 text-gray-800">
         <Button type="submit" color="blue" loading={loading}>
           Salvar

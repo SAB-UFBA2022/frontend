936d3f73fa490cce20b4ac05b6a90a2dc905c973
brunodelucasbarbosa <brunnobarbosas@gmail.com>
2022-10-11 17:36:42 -0300

    instalando mui

--- src/pages/Docente/Lista/index.jsx
+++ src/pages/Docente/Lista/index.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import { useEffect } from 'react'
-import { useAppContext } from '../../../context/appContext'
-import Sidebar from '../../../components/Sidebar/Docente'
-import Loading from '../../../components/Loading'
-import Paginacao from '../../../components/Paginacao'
+import { DataGrid } from '@mui/x-data-grid'
 import Filter from '../../../components/Filter'
+import Paginacao from '../../../components/Paginacao'
+import Loading from '../../../components/Loading'
+import Sidebar from '../../../components/Sidebar/Docente'
+import { useAppContext } from '../../../context/appContext'
 
 export default function DiscenteLista() {
   const {
@@ -55,6 +57,38 @@ export default function DiscenteLista() {
     return 'Todos'
   }
 
+  const columns = [
+    { field: 'Nome', headerName: 'Nome', width: 70 },
+    { field: 'Telefone', headerName: 'Telefone', width: 130 },
+    { field: 'lastName', headerName: 'Matrícula', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`
+    }
+  ]
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 }
+  ]
+
   return (
     <div className="flex h-screen flex-col overflow-auto bg-gray-100 md:flex-row">
       <Sidebar />
@@ -65,6 +99,15 @@ export default function DiscenteLista() {
             <Loading />
           ) : (
             <>
+              <div style={{ height: 400, width: '100%' }}>
+                <DataGrid
+                  rows={rows}
+                  columns={columns}
+                  pageSize={5}
+                  rowsPerPageOptions={[5]}
+                  checkboxSelection
+                />
+              </div>
               <h3 className="font-poppins text-lg font-medium text-gray-800">
                 {totalItems} Resultado{students.length > 1 && 's'} encontrado
                 {students.length > 1 && 's'}: {formatType(selectedItem)}{' '}

2d7d19258e5e70dd724bed4189fc133046163677
Monique Silva <moniquesntsilva@gmail.com>
2022-10-12 22:36:23 -0300

    ajuste no estilo e add email

--- src/pages/Docente/Lista/index.jsx
+++ src/pages/Docente/Lista/index.jsx
@@ -39,15 +39,23 @@ export default function DiscenteLista() {
   }, [currentPage, allItems, courseType, scholarshipDate, sort])
 
   const columns = [
-    { field: 'name', headerName: 'Nome Completo', width: 190 },
+    {
+      field: 'name',
+      headerName: 'Nome Completo',
+      width: 200,
+      renderCell: (params) => (
+        <div className="flex items-center gap-x-2">
+          <img src="/assets/icons/profile.svg" alt="Profile" />
+          {params.row.name}
+        </div>
+      )
+    },
     { field: 'course', headerName: 'Curso', width: 100 },
     {
-      field: 'phone_number',
-      headerName: 'Telefone',
-      width: 150,
-      renderCell: (params) => formatPhone(params.row.phone_number)
+      field: 'enrollment_number',
+      headerName: 'Matrícula',
+      width: 120
     },
-    { field: 'enrollment_number', headerName: 'Matrícula', width: 120 },
     {
       field: 'enrollment_date_pgcomp',
       headerName: 'Data de Matrícula',
@@ -72,7 +80,41 @@ export default function DiscenteLista() {
       width: 150,
       renderCell: (params) => formatDate(params.row.defense_prediction)
     },
-    { field: 'link_lattes', headerName: 'Lattes', width: 100 }
+    { field: 'email', headerName: 'E-mail', width: 200 },
+    {
+      field: 'phone_number',
+      headerName: 'Telefone',
+      width: 150,
+      renderCell: (params) => formatPhone(params.row.phone_number)
+    },
+    {
+      field: 'link_lattes',
+      headerName: 'Lattes',
+      width: 70,
+      align: 'center',
+      renderCell: (params) => (
+        <a
+          href={params.row.link_lattes}
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 underline"
+        >
+          Link
+        </a>
+      )
+    },
+    {
+      field: 'active',
+      headerName: 'Bolsa ativa',
+      width: 100,
+      align: 'center',
+      renderCell: (params) =>
+        params.row.scholarship.active ? (
+          <p className="text-green-500">Sim</p>
+        ) : (
+          <p className="text-red-500">Não</p>
+        )
+    }
   ]
 
   return (
@@ -80,24 +122,32 @@ export default function DiscenteLista() {
       <Sidebar />
       <section className="w-full py-6 md:ml-auto md:max-w-[70vw] xl:max-w-[80vw]">
         <div className="shadow-base mb-6 w-full space-y-8 lg:w-10/12">
-          {isLoading ? (
-            <Loading />
-          ) : (
-            <div style={{ height: '77vh', width: '100%', backgroundColor: 'white' }}>
-              <DataGrid
-                hideFooterPagination
-                rows={students}
-                columns={columns}
-                pageSize={10}
-                rowsPerPageOptions={[10]}
-                disableColumnMenu
-                GridCellParams={students.scholarship}
-                isRowSelectable={() => false}
-                rowHeight={60}
-                hideFooter
-              />
+          <div className="shadow-base rounded-lg bg-white p-8">
+            <div className="mb-8 flex items-center gap-x-4">
+              <div className="rounded-md bg-yellow-500 p-3">
+                <img src="/assets/icons/list.svg" alt="Lista" />
+              </div>
+              <h2 className="poppins text-xl font-semibold text-gray-900">Lista de Estudantes</h2>
             </div>
-          )}
+            {isLoading ? (
+              <Loading />
+            ) : (
+              <div style={{ height: '675px', width: '100%', backgroundColor: 'white' }}>
+                <DataGrid
+                  hideFooterPagination
+                  rows={students}
+                  columns={columns}
+                  pageSize={10}
+                  rowsPerPageOptions={[10]}
+                  disableColumnMenu
+                  GridCellParams={students.scholarship}
+                  isRowSelectable={() => false}
+                  rowHeight={60}
+                  hideFooter
+                />
+              </div>
+            )}
+          </div>
           <Paginacao />
         </div>
       </section>

f50a425ab7b39cbc6b0c84e4c08fd9a5b98f15a2
Monique Silva <moniquesntsilva@gmail.com>
2022-10-21 23:59:50 -0300

    tirar background azul

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -50,7 +50,6 @@ export default function Login() {
 
   return (
     <div className="flex h-screen w-full items-center justify-center p-6 md:p-0">
-      <div role="img" className="hidden h-screen bg-sky-200 md:block md:w-1/12 lg:w-6/12" />
       <section className="flex w-full flex-col items-center justify-center gap-y-7 md:w-11/12 lg:w-6/12">
         <img src="/assets/logo.png" alt="Logo" className="w-full max-w-[395px]" />
         <h1 className="text-center font-poppins text-2xl font-semibold leading-8 text-gray-900">

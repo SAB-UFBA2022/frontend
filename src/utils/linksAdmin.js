24e0ffb6253e115bb6555b146b7184893566429a
Monique Silva <moniquesntsilva@gmail.com>
2022-10-22 14:14:40 -0300

    add links e bolsas expiradas

---
+++ src/utils/linksAdmin.js
@@ -0,0 +1,17 @@
+export const linksAdmin = [
+  {
+    name: 'Dashboard',
+    icon: 'home',
+    path: '/admin/dashboard'
+  },
+  {
+    name: 'Estudantes',
+    icon: 'list',
+    path: '/admin/lista'
+  },
+  {
+    name: 'Bolsas Expiradas',
+    icon: 'cancel',
+    path: '/admin/bolsas/expiradas'
+  }
+]

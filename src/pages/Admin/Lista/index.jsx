c2e681f4907bee6fe908790cb3e20586e0ca859c
Monique Silva <moniquesntsilva@gmail.com>
2022-10-29 17:21:26 -0300

    ajustes no conteiner

--- src/pages/Admin/Lista/index.jsx
+++ src/pages/Admin/Lista/index.jsx
@@ -5,20 +5,18 @@ export default function DiscenteLista() {
   return (
     <div className="flex h-screen flex-col overflow-auto bg-gray-100 md:flex-row">
       <Sidebar userType="admin" />
-      <section className="w-full py-6 md:ml-auto md:max-w-[70vw] xl:max-w-[80vw]">
-        <div className="shadow-base mb-6 w-full space-y-8 lg:w-10/12">
-          <div className="shadow-base rounded-lg bg-white p-8">
-            <div className="mb-8 flex items-center gap-x-4">
-              <div className="rounded-md bg-blue-400 p-3">
-                <img src="/assets/icons/list.svg" alt="Lista" />
-              </div>
-              <div>
-                <h2 className="poppins text-xl font-semibold text-gray-900">Lista de Estudantes</h2>
-                <p className="poppins font-medium text-gray-500">Ordem de finalização</p>
-              </div>
+      <section className="flex w-full justify-center py-6">
+        <div className="shadow-base h-max w-full space-y-8 rounded-lg bg-white p-6 lg:max-w-[1120px]">
+          <div className="mb-8 flex items-center gap-x-4">
+            <div className="rounded-md bg-blue-400 p-3">
+              <img src="/assets/icons/list.svg" alt="Lista" />
             </div>
-            <List listType="finalizacao" />
+            <div>
+              <h2 className="poppins text-xl font-semibold text-gray-900">Lista de Estudantes</h2>
+              <p className="poppins font-medium text-gray-500">Ordem de finalização</p>
+            </div>
           </div>
+          <List listType="finalizacao" />
         </div>
       </section>
     </div>

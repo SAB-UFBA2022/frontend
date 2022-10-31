cc198d4ed8e7abf7b5ecdc97fef7f082302a33ab
Monique Silva <moniquesntsilva@gmail.com>
2022-10-30 21:44:42 -0300

    troca alguns detalhes de layout

--- src/pages/ForgetPassword/index.jsx
+++ src/pages/ForgetPassword/index.jsx
@@ -3,16 +3,16 @@ import ForgetPasswordForm from '../../components/ForgetPasswordForm'
 export default function ForgetPassword() {
   return (
     <div className="flex h-screen w-full items-center justify-center p-6 md:p-0">
-      <div role="img" className="hidden h-screen bg-sky-200 md:block md:w-1/12 lg:w-6/12" />
-      <section className="  flex w-full flex-col items-center justify-center gap-y-7 md:w-11/12 lg:w-6/12">
-        <img src="/assets/logo.png" alt="Logo" className="w-full max-w-[395px]" />
-        <h1 className="text-center font-poppins text-2xl font-semibold leading-8 text-gray-900">
-          Acompanhamento de Bolsistas
+      <section className="flex w-full flex-col items-center justify-center gap-y-7 md:w-11/12 lg:w-full">
+        <img src="/assets/logo.png" alt="Logo" className="w-full max-w-[390px]" />
+        <h1 className="max-w-[390px] text-center font-poppins text-lg font-semibold leading-7 text-gray-900 md:w-11/12 lg:w-6/12 lg:text-xl">
+          <a href="/" className="w-full">
+            Sistema de acompanhamento de bolsistas do PGCOMP
+          </a>
         </h1>
-        <h2 className="max-w-[400px] text-center font-inter font-semibold leading-8 text-gray-900">
-          Forneça seu endereço de e-mail usando durante o seu cadastro e enviaremos sua senha
-          recuperada.
-        </h2>
+        <p className="mt-4 max-w-[390px] text-center font-inter text-sm leading-6 text-gray-800 md:text-base">
+          Informe seu endereço de e-mail usando durante o cadastro e lhe enviaremos uma nova senha.
+        </p>
         <ForgetPasswordForm />
       </section>
     </div>

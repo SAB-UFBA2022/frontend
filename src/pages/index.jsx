ed2b211d078ad957bea2332693043c9b5e9eeff6
Monique Silva <moniquesntsilva@gmail.com>
2022-11-20 21:38:59 -0300

    add relatorios

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import AdminDashboard from './Admin/Dashboard'
 import AdminBolsasExpiradas from './Admin/BolsasExpiradas'
 import AdminLista from './Admin/Lista'
+import AdminRelatorios from './Admin/Relatorios'
 import DiscenteDashboard from './Discente/Dashboard'
 import DocenteDashboard from './Docente/Dashboard'
 import DocenteLista from './Docente/Lista'
 import DocenteBolsasExpiradas from './Docente/BolsasExpiradas'
+import DocenteRelatorios from './Docente/Relatorios'
 import Login from './Login'
 import Page404 from './Page404'
 import ForgetPassword from './ForgetPassword'
@@ -13,10 +15,12 @@ export {
   AdminDashboard,
   AdminBolsasExpiradas,
   AdminLista,
+  AdminRelatorios,
   DiscenteDashboard,
   DocenteLista,
   DocenteDashboard,
   DocenteBolsasExpiradas,
+  DocenteRelatorios,
   Login,
   Page404,
   ForgetPassword
